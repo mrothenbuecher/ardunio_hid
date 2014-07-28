6b4fcd4da84486ec9313b07dae848e10da08e401
Michael Kürbis <michael.kuerbis@gmx.de>
2014-07-28 23:35:13 +0200

    added few codes

--- sketch_hid.ino
+++ sketch_hid.ino
@@ -23,68 +23,61 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  */
-
-/*
-When we send data to the keyboard driver, it has to be
- scancodes, not ascii characters. Refer to the AVR keyboard
- source code to obtain the scancodes. 
- Notes:
- -Modifiers (Shift, Alt, Ctrl) only modify the next character.
- -When sending modifiers, the byte that is sent must be the amount
- of shift (referenced in the source code) plus 0xf0.
- -Only one modifier can be used at a time.
- -Once a modifier is sent, it will remain on until one of two things
- happen: another byte is sent, or the same modifier is pressed on 
- another keyboard connected to the system.
- -A delay of at least 25 milliseconds is required after each byte in
- order to gice the keyboard driver time to respond. A function to 
- take care of this might be needed.
- */
-
 // Letter keys
-byte SC_A = 0x04;            
-byte SC_B = 0x05;            
-byte SC_C = 0x06;
-byte SC_D = 0x07;
-byte SC_E = 0x08;
-byte SC_F = 0x09;
-byte SC_G = 0x0a;
-byte SC_H = 0x0b;
-byte SC_I = 0x0c;
-byte SC_J = 0x0d;
-byte SC_K = 0x0e;
-byte SC_L = 0x0f;
-byte SC_M = 0x10;
-byte SC_N = 0x11;
-byte SC_O = 0x12;
-byte SC_P = 0x13;
-byte SC_Q = 0x14;
-byte SC_R = 0x15;
-byte SC_S = 0x16;
-byte SC_T = 0x17;
-byte SC_U = 0x18;
-byte SC_V = 0x19;
-byte SC_W = 0x1a;
-byte SC_X = 0x1b;
-byte SC_Y = 0x1c;
-byte SC_Z = 0x1d;
+const byte SC_A = 0x04;            
+const byte SC_B = 0x05;            
+const byte SC_C = 0x06;
+const byte SC_D = 0x07;
+const byte SC_E = 0x08;
+const byte SC_F = 0x09;
+const byte SC_G = 0x0a;
+const byte SC_H = 0x0b;
+const byte SC_I = 0x0c;
+const byte SC_J = 0x0d;
+const byte SC_K = 0x0e;
+const byte SC_L = 0x0f;
+const byte SC_M = 0x10;
+const byte SC_N = 0x11;
+const byte SC_O = 0x12;
+const byte SC_P = 0x13;
+const byte SC_Q = 0x14;
+const byte SC_R = 0x15;
+const byte SC_S = 0x16;
+const byte SC_T = 0x17;
+const byte SC_U = 0x18;
+const byte SC_V = 0x19;
+const byte SC_W = 0x1a;
+const byte SC_X = 0x1b;
+const byte SC_Y = 0x1c;
+const byte SC_Z = 0x1d;
 // Controll keys
-byte SC_ESCAPE = 0x29;
-byte SC_TAB = 0x2b;
-byte SC_BCKSPC = 0x2a;
-byte SC_SPACE = 0x2c;
-byte SC_ENTER = 0x28;
+const byte SC_ESCAPE = 0x29;
+const byte SC_TAB = 0x2b;
+const byte SC_BCKSPC = 0x2a;
+const byte SC_SPACE = 0x2c;
+const byte SC_ENTER = 0x28;
 // Mod keys
-byte MOD_CTRL = 0xf0;
-byte MOD_ALT = 0xf2;
-byte MOD_SHIFT = 0xf1;
+const byte MOD_CTRL = 0xf0;
+const byte MOD_ALT = 0xf2;
+const byte MOD_SHIFT = 0xf1;
 // Function Keys
-
+const byte SC_F1 = 0x3a;
+const byte SC_F2 = 0x3b;
+const byte SC_F3 = 0x3c;
+const byte SC_F4 = 0x3d;
+const byte SC_F5 = 0x3e;
+const byte SC_F6 = 0x3f;
+const byte SC_F7 = 0x40;
+const byte SC_F8 = 0x41;
+const byte SC_F9 = 0x42;
+const byte SC_F10 = 0x43;
+const byte SC_F11 = 0x44;
+const byte SC_F12 = 0x45;
 // Arrow keys
-byte SC_UP_ARROW = 0x52;
-byte SC_DOWN_ARROW = 0x51;
-byte SC_LEFT_ARROW = 0x50;
-byte SC_RIGHT_ARROW = 0xef;
+const byte SC_UP_ARROW = 0x52;
+const byte SC_DOWN_ARROW = 0x51;
+const byte SC_LEFT_ARROW = 0x50;
+const byte SC_RIGHT_ARROW = 0xef;
 
 void setup()
 {
@@ -94,6 +87,7 @@ void setup()
 
 void loop()
 {
+  // example code
   sendSCByte(MOD_SHIFT);
   sendSCByte(SC_A);
   sendSCByte(SC_R);
